134866b936bdf77968be8818155ca91669f3d936
Kashish keshri <132193072+Kashishkeshri@users.noreply.github.com>
2024-10-19 02:06:55 +0530

    Add files via upload
    In first commit I have created the menu of the restaurant.

---
+++ hotelmenue.py
@@ -0,0 +1,7 @@
+#define a menue of restaurant
+menu ={'Pizza':50,'Burger':30,'noodles':60,'Pasta':80,'Cold_Coffee':50,'cold_drink':40}
+
+#greet
+print("Welcom to our restaurant")
+print("The menue is :-\n")
+print("Pizza:Rs50\nBurger:Rs30\nnoodles:Rs60\nPasta:Rs80\nCold_Coffee:Rs50\ncold_drink:Rs40")
